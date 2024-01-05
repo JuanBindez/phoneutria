42ab280e0052ffd280c5b052d63e616bb8b02b69
JuanBindez <juanbindez780@gmail.com>
2024-01-04 22:30:45 -0300

    update

--- phoneutria/google_hacking.py
+++ phoneutria/google_hacking.py
@@ -2,7 +2,7 @@ from googlesearch import search
 
 def google_search(query, num_results=5):
     try:
-        results = search(query, num_results=num_results, stop=num_results)
+        results = search(query, num_results=num_results)
 
         for i, result in enumerate(results, 1):
             print(f"{i}. {result}")
